f80a3b4de8d5a5091a6d714f71a6d77775212400
rockgroupit777 <54498741+rockgroupit777@users.noreply.github.com>
2024-10-12 12:47:08 +0700

    a

--- server/src/graphql/index.ts
+++ server/src/graphql/index.ts
@@ -1,4 +1,10 @@
-import {typeDefs} from './schema';
-import {resolvers} from './resolvers';
+const { mergeTypeDefs } = require('@graphql-tools/merge');
+import fs from 'fs';
+import path from 'path';
 
-export {typeDefs,resolvers}
+// Đọc và gộp schema từ các file nhỏ
+const typeDefs = mergeTypeDefs([
+  fs.readFileSync(path.join(__dirname, 'user.graphql'), { encoding: 'utf-8' }),
+  fs.readFileSync(path.join(__dirname, 'post.graphql'), { encoding: 'utf-8' }),
+]);
+export typeDefs;
